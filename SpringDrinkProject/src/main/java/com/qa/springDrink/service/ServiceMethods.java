a21e447966ddfd431c6bfb941a9f18c994d8d493
GitHub Idris <idristayi1@yahoo.com>
2022-01-06 18:24:05 +0000

    Completed work on the serviceBranch

--- SpringDrinkProject/src/main/java/com/qa/springDrink/service/ServiceMethods.java
+++ SpringDrinkProject/src/main/java/com/qa/springDrink/service/ServiceMethods.java
@@ -1,5 +1,22 @@
 package com.qa.springDrink.service;
+import java.util.List;
 
-public interface ServiceMethods {
-
+public interface ServiceMethods <T> {
+	
+	// CRUD METHODS
+	
+	//CREATE
+	T create (T t);
+	
+	//Read All
+	List<T> getAll();
+	
+	//Read By Id
+	T getById(long id);
+	
+	//UPDATE
+	T update(long id, T t);
+	
+	//DELETE
+	boolean delete(long id);
 }
